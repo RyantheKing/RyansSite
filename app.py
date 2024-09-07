84bec144925e2e30a7e018587bc857faf4fad1d8
RyantheKing <gamingskills2002@gmail.com>
2024-09-07 13:26:08 -0700

    update ports for https

--- app.py
+++ app.py
@@ -77,4 +77,4 @@ app.mount("/", StaticFiles(directory="site", html=True), name="static")
     
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=4443)
