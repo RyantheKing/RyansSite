80ab9f60480665a2e80933377d2ba76765a32e94
Ryan <rytking@ucsc.edu>
2025-11-13 15:55:01 -0800

    avoid spawning on checkboxes

--- assets/js/oneko.js
+++ assets/js/oneko.js
@@ -19,7 +19,7 @@ function generateSpawn(minH, minV, maxH, maxV) {
   do {
     x = randomCoord(minH, maxH);
     y = randomCoord(minV, maxV);
-  } while (isOnTopOfAnyElement(x, y, "p, a, i, h1, h2, h3, h4, h5, h6, img"))
+  } while (isOnTopOfAnyElement(x, y, "p, a, i, h1, h2, h3, h4, h5, h6, img, label, input"))
   return [x, y];
 }
 
